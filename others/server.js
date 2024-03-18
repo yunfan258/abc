0cea0094d8fdcade268175c97d52c7cd44d5a989
luofan <1349891253@qq.com>
2024-03-18 18:48:24 +0800

    改变京东地址

---
+++ others/server.js
@@ -0,0 +1,57 @@
+// var http = require('http');
+
+// http.createServer(function (request, response) {
+
+//     // 发送 HTTP 头部 
+//     // HTTP 状态值: 200 : OK
+//     // 内容类型: text/plain
+//     response.writeHead(200, {'Content-Type': 'text/plain'});
+
+//     // 发送响应数据 "Hello World"
+//     response.end('Hello World\n');
+// }).listen(8888);
+
+// // 终端打印如下信息
+// console.log('Server running at http://127.0.0.1:8888/');
+
+// var fs = require("fs");
+
+// var data = fs.readFileSync('input.txt');
+
+// console.log(data.toString());
+// console.log("程序执行结束!");
+
+var http = require('http');
+var querystring = require('querystring');
+ 
+var postHTML = 
+  '<html><head><meta charset="utf-8"><title>菜鸟教程 Node.js 实例</title></head>' +
+  '<body>' +
+  '<form method="post">' +
+  '网站名： <input name="name"><br>' +
+  '网站 URL： <input name="url"><br>' +
+  '<input type="submit">' +
+  '</form>' +
+  '</body></html>';
+ 
+http.createServer(function (req, res) {
+  var body = "";
+  req.on('data', function (chunk) {
+    body += chunk;
+  });
+//   req.on('end', function () {
+//     // 解析参数
+//     body = querystring.parse(body);
+//     // 设置响应头部信息及编码
+//     res.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
+ 
+//     if(body.name && body.url) { // 输出提交的数据
+//         res.write("网站名：" + body.name);
+//         res.write("<br>");
+//         res.write("网站 URL：" + body.url);
+//     } else {  // 输出表单
+//         res.write(postHTML);
+//     }
+//     res.end();
+//   });
+}).listen(3000);
